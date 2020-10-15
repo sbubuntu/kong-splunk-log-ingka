a3e04c6a121c4243e5007576912b79a2f2e4c0c4
soumitra-bhattacharyya <soumitra.bhattacharyya@ingka.ikea.com>
2020-10-16 01:08:50 +0200

    0.3-43

--- src/basic.lua
+++ src/basic.lua
@@ -50,7 +50,7 @@ function _M.serialize(ngx, conf, sessionId, kong)
         ApiRequest = {
           CID = req.get_headers()["optum-cid-ext"],
           UniqueID = sessionId,
-          XClientId = kong.request.get_headers()[x-client-id],
+          XClientID = kong.request.get_headers()[x-client-id],
           Env = conf.apim_env,
           WorkSpace = conf.workspace,
           HTTPMethod = kong.request.get_method(),
@@ -89,7 +89,7 @@ function _M.serialize(ngx, conf, sessionId, kong)
         ApiResponse = {   
           CID = req.get_headers()["optum-cid-ext"],
           UniqueID = sessionId,
-          XClientId = kong.request.get_headers()[x-client-id],
+          XClientID = kong.request.get_headers()[x-client-id],
           Env = conf.apim_env,
           WorkSpace = conf.workspace,
           HTTPMethod = kong.request.get_method(),
